653c7ed3f6aa7f516280a652ae805bb4314aa4b5
Dustin Clark <dustin@captivatelabs.com>
2022-10-20 13:57:32 -0500

    Attempt setting switch names through characteristic.

--- src/partitionAccessory.ts
+++ src/partitionAccessory.ts
@@ -48,6 +48,7 @@ export class EnvisalinkPartitionAccessory {
             this.accessory.addService(this.platform.Service.Switch, CHIME_SERVICE_NAME, `${this.partition.number}-Chime`);
         chimeService.displayName = CHIME_SERVICE_NAME;
         chimeService.subtype = CHIME_SERVICE_NAME;
+        chimeService.setCharacteristic(this.platform.Characteristic.Name, CHIME_SERVICE_NAME);
         chimeService.getCharacteristic(this.platform.Characteristic.On)
             .onSet(this.setChimeActive.bind(this));
         chimeService.updateCharacteristic(this.platform.Characteristic.On,
@@ -59,6 +60,7 @@ export class EnvisalinkPartitionAccessory {
             this.accessory.addService(this.platform.Service.Switch, BYPASS_SERVICE_NAME, `${this.partition.number}-Bypass`);
         bypassService.displayName = BYPASS_SERVICE_NAME;
         bypassService.subtype = BYPASS_SERVICE_NAME;
+        bypassService.setCharacteristic(this.platform.Characteristic.Name, BYPASS_SERVICE_NAME);
         bypassService.getCharacteristic(this.platform.Characteristic.On)
             .onSet(this.setBypassActive.bind(this));
         bypassService.updateCharacteristic(this.platform.Characteristic.On,
